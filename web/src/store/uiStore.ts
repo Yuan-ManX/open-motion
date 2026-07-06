6db3ec4243ddb28eac5b162334838b4eaaa0b388
Yuan-Man <ym1076302261@163.com>
2026-07-06 10:42:46 +0800

    fix: update uiStore

--- web/src/store/uiStore.ts
+++ web/src/store/uiStore.ts
@@ -8,6 +8,8 @@ interface UiState {
   skillsOpen: boolean;
   health: HealthResponse | null;
   replayTrigger: number;
+  hiddenIds: Set<string>;
+  canvasSize: { width: number; height: number };
 
   selectComponent: (id: string | null) => void;
   setExportOpen: (open: boolean) => void;
@@ -15,15 +17,19 @@ interface UiState {
   setSkillsOpen: (open: boolean) => void;
   setHealth: (h: HealthResponse | null) => void;
   triggerReplay: () => void;
+  toggleHidden: (id: string) => void;
+  setCanvasSize: (size: { width: number; height: number }) => void;
 }
 
-export const useUiStore = create<UiState>((set) => ({
+export const useUiStore = create<UiState>((set, get) => ({
   selectedComponentId: null,
   exportOpen: false,
   templatesOpen: false,
   skillsOpen: false,
   health: null,
   replayTrigger: 0,
+  hiddenIds: new Set<string>(),
+  canvasSize: { width: 640, height: 360 },
 
   selectComponent: (id) => set({ selectedComponentId: id }),
   setExportOpen: (open) => set({ exportOpen: open }),
@@ -31,4 +37,11 @@ export const useUiStore = create<UiState>((set) => ({
   setSkillsOpen: (open) => set({ skillsOpen: open }),
   setHealth: (h) => set({ health: h }),
   triggerReplay: () => set((s) => ({ replayTrigger: s.replayTrigger + 1 })),
+  toggleHidden: (id) => {
+    const next = new Set(get().hiddenIds);
+    if (next.has(id)) next.delete(id);
+    else next.add(id);
+    set({ hiddenIds: next });
+  },
+  setCanvasSize: (size) => set({ canvasSize: size }),
 }));
