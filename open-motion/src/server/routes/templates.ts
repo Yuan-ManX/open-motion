058ae851504d579b9fcc7a0c902afc43e03b9aa0
Yuan-Man <ym1076302261@163.com>
2026-07-10 09:50:36 +0800

    fix: update templates

--- open-motion/src/server/routes/templates.ts
+++ open-motion/src/server/routes/templates.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
 import { runAsync } from "../../utils/async.js";
 import { listAllTemplates, getTemplateOrThrow } from "../services/templateService.js";
+import { getCategories } from "../../motion/templates/index.js";
+import { listStylePresets } from "../../motion/stylePresets.js";
 
 export const templatesRouter = Router();
+
+templatesRouter.get(
+  "/style-presets",
+  runAsync(async (_req, res) => {
+    res.json(listStylePresets());
+  }),
+);
 
 templatesRouter.get(
   "/templates",
@@ -10,6 +19,13 @@ templatesRouter.get(
     const category = typeof req.query.category === "string" ? req.query.category : undefined;
     const tag = typeof req.query.tag === "string" ? req.query.tag : undefined;
     res.json(listAllTemplates(category, tag));
+  }),
+);
+
+templatesRouter.get(
+  "/templates/categories",
+  runAsync(async (_req, res) => {
+    res.json(getCategories());
   }),
 );
 
